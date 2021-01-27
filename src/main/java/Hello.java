6ef6959472db306f66377f2f91d90021e83f3a82
gulis221 <gulis221@gmail.com>
2021-01-26 23:21:10 -0500

    final work on feature c

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("future sdet");
+        System.out.println("Hello worl b21");
+
     }
+
+
+
+
+
 }
